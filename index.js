c847c9294e164d5476a410a9f8799764795ca36d
Sami Humalajoki <samhum@utu.fi>
2020-03-24 11:07:03 +0200

    chat

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ io.on('connection', function(socket){
   });
 });
 
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+const PORT = process.env.PORT || 3000
+http.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
