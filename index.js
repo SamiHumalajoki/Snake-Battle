f02771bb21010b5910c3e6dbf13b2c8e220cac2c
Sami Humalajoki <samhum@utu.fi>
2020-03-31 12:04:14 +0300

    first 'playable' version

--- index.js
+++ index.js
@@ -5,15 +5,46 @@ const io = require('socket.io')(http);
 
 const PORT = process.env.PORT || 3000;
 
+var player1;
+var player2;
+
 http.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 app.use(express.static('public'))
 
 io.on('connection', (socket) => {
-  console.log(`${socket.id} connected`);
+  if (player1 === undefined) {
+    console.log(`${socket.id} connected as player 1`);
+    player1 = socket.id;
+    if (player2 !== undefined) {
+      io.emit('startGame');
+    }
+  }
+  else if (player2 === undefined) {
+    console.log(`${socket.id} connected as player 2`);
+    player2 = socket.id;
+    if (player1 !== undefined) {
+      io.emit('startGame');
+    }
+  }
+
+  socket.on('hit', () => {
+    socket.broadcast.emit('hit');
+  })
+ 
   socket.on('disconnect', () => {
     console.log(`${socket.id} disconnected`);
+    if (socket.id === player1) {
+      console.log(`player 1 left the game`);
+      player1 = undefined;
+      io.emit('stopGame');
+    }
+    if (socket.id === player2) {
+      console.log(`player 2 left the game`);
+      player2 = undefined;
+      io.emit('stopGame');
+    }
   })
   socket.on('move', (data) => {
     //console.log(data);
--- public/sketch.js
+++ public/sketch.js
@@ -1,62 +1,94 @@
-var socket;
-var otherX;
-var otherY;
-var vx = 1;
-var vy = 0;
-var blockSize = 10;
-var x;
-var y;
+let socket;
+let opponentX;
+let opponentY;
+let vx = 1;
+let vy = 0;
+let blockSize = 10;
+let x;
+let y;
+let gridSizeX = 50;
+let gridSizeY = 50;
+let gameOn = false;
+let mySnake = [];
+let opponentSnake = [];
+let startSequence = 0;
+let timeCount;
+let moveInterval = 500;
 
 function setup() {
-  createCanvas(500, 500);
-  frameRate(10);
-  background(51);
-  x = round(random(100,400));
-  y = round(random(100,400));
+  createCanvas(gridSizeX * blockSize, gridSizeY * blockSize);
   socket = io();
-  socket.on('move', (data) => {
-    otherX = data.x;
-    otherY = data.y;
+  socket.on('startGame', () => {
+    console.log('game started');
+    startGame();
   });
+  socket.on('stopGame', () => {
+    console.log('game ended');
+    gameOn = false;
+  });
+  socket.on('move', (data) => {
+    opponentSnake.push({x: data.x, y:data.y});
+    opponentX = data.x;
+    opponentY = data.y;
+  });
+  socket.on('hit', () => {
+    console.log('opponent hit');
+    startGame();
+  })
 }
 
 function keyPressed() {
   console.log(keyCode);
-  if (keyCode == LEFT_ARROW) {
+  if (keyCode === LEFT_ARROW && vx !== 1) {
     vx = -1;
     vy = 0;
   }
-  if (keyCode == RIGHT_ARROW) {
+  if (keyCode === RIGHT_ARROW && vx !== -1) {
     vx = 1;
     vy = 0;
   }
-  if (keyCode == UP_ARROW) {
+  if (keyCode === UP_ARROW && vy !== 1) {
     vx = 0;
     vy = -1;
   }
-  if (keyCode == DOWN_ARROW) {
+  if (keyCode === DOWN_ARROW && vy !== -1) {
     vx = 0;
     vy = 1;
   }
 }
 
-/*
-function touchMoved() {
-  const data = {
-    x: mouseX,
-    y: mouseY
-  }
-  socket.emit('mouse', data);
+function startGame() {
+  opponentSnake = [];
+  mySnake = [];
+  background(51);
+  x = round(random(10,40)) * blockSize;
+  y = round(random(10,40)) * blockSize;
+  gameOn = true;
+  timeCount = millis();
 }
-*/
 
 function draw() {
-  //background(51);
-  x += vx * blockSize;
-  y += vy * blockSize;
-  socket.emit('move', {x:x, y: y});
-  rect(x, y, blockSize, blockSize);
-  if(otherX) {
-    rect(otherX, otherY, blockSize, blockSize);
+  if (gameOn && timeCount + moveInterval < millis() && mySnake.length < opponentSnake.length + 1) {
+    timeCount = millis();
+    x += vx * blockSize;
+    y += vy * blockSize;
+    if (opponentSnake.find(block => (x === block.x && y === block.y)) ||
+        mySnake.find((block, index) => (index < mySnake.length - 1 && x === block.x && y === block.y))) {
+          socket.emit('hit');
+          console.log('hit');
+          startGame();
+        } 
+    mySnake.push({x: x, y: y});
+    //console.log(`x: ${x}, y:${y}`);
+    socket.emit('move', {x:x, y: y});
+    fill(color('red'));
+    rect(x, y, blockSize, blockSize);
+    if(opponentX) {
+      fill(color('white'));
+      rect(opponentX, opponentY, blockSize, blockSize);
+    }
+  }
+  else if (startSequence !== 0) {
+
   }
 }
